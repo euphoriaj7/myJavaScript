72499ece1c9c69a48adfdaa578825e29ae866a23
Jeff Pitcher <pit19004@byui.edu>
2022-05-03 15:58:26 -0600

    more code added

--- notes.js
+++ notes.js
@@ -1,7 +1,12 @@
+// 5-3-22
 const nums = [1, 2, 3, 4, 5];
 
 function double(number) {
     console.log(number * 2);
     return number * 2;
 }
-nums.forEach(double);
+// nums.forEach(double);
+
+// nums.forEach((number => number * 2));
+
+const newArray = nums.map(double);
