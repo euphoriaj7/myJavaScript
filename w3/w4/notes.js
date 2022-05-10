4980827b90a0f71bb4e4427210a542b10604aa1f
Jeff Pitcher <pit19004@byui.edu>
2022-05-10 15:37:40 -0600

    made week 3 and  week 4 folder

---
+++ w3/w4/notes.js
@@ -0,0 +1,20 @@
+// example of a function
+let averageTwoNubmers = function(firstNum, secondNum) {
+    let sum = firstNum + secondNum;
+    let avg = sum / 2;
+    return avg;
+}
+
+// 2nd exmaple of another function 
+let averageTwoNumbers = (firstNum, secondNum) => {
+    let sum = firstNum + secondNum;
+    let avg = sum / 2;
+    return avg;
+}
+
+// third example
+function averageTwoNubmers(firstNum, secondNum) {
+    let sum = firstNum + secondNum;
+    let avg = sum / 2;
+    return avg;
+}
