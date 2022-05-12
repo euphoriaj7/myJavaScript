5abe7d66edd55cd1871767e87cde1253755d96df
Jeff Pitcher <pit19004@byui.edu>
2022-05-12 15:38:44 -0600

    2nd push

--- w4/courses.js
+++ w4/courses.js
@@ -1,5 +1,25 @@
 // courses.js
 const aCourse = {
-    code: "CSE121b",
+    courseCode: "CSE121b ",
     name: "Javascript Language",
+    sections: [{
+            sectionNum: 1,
+            roomNum: 'STC 353',
+            enrolled: 26,
+            days: 'TTh',
+            instructor: 'Bro T'
+        },
+        {
+            sectionNum: 2,
+            roomNum: 'STC 347',
+            enrolled: 28,
+            days: 'TTh',
+            instructor: 'Sis A'
+        }
+    ]
 };
+
+function setCourseInfo(course) {
+    document.querySelector('#courseName')
+    document.querySelector('#courseCode')
+}
