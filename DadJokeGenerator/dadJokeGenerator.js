9d68016c92409756b5c316be571a2c3bc136354b
Jeff Pitcher <pit19004@byui.edu>
2022-05-24 17:41:25 -0600

    button and welcome message on screen

--- DadJokeGenerator/dadJokeGenerator.js
+++ DadJokeGenerator/dadJokeGenerator.js
@@ -1,4 +1,12 @@
 'use strict';
-const url = "https://rapidapi.com/KegenGuyll/api/dad-jokes";
-const pull = fetch(url, { mode: "no-cors" });
-console.log(pull);
+
+
+function tellJoke() {
+
+}
+
+function fetchJoke() {
+    const url = "https://rapidapi.com/KegenGuyll/api/dad-jokes";
+    const pull = fetch(url, { mode: "no-cors" });
+    console.log(pull);
+}
