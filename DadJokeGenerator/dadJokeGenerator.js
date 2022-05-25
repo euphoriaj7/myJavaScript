c53ac13d13af907430ab290371f091f3e7090f6f
Jeff Pitcher <pit19004@byui.edu>
2022-05-24 21:33:32 -0600

    week 6 update

--- DadJokeGenerator/dadJokeGenerator.js
+++ DadJokeGenerator/dadJokeGenerator.js
@@ -2,11 +2,18 @@
 
 
 function tellJoke() {
-
+    const display = document.querySelector("#displayJoke");
+    // console.log(display)
+    display.append(fetchJoke())
 }
 
-function fetchJoke() {
-    const url = "https://rapidapi.com/KegenGuyll/api/dad-jokes";
-    const pull = fetch(url, { mode: "no-cors" });
+async function fetchJoke() {
+    // console.log("Hello World");
+    const pull = await fetch("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } })
+        // const url = "https://dad-jokes.p.rapidapi.com/random/joke";
+        // const pull = await fetch(url, { mode: "no-cors" });
+    const tellJoke = tellJoke();
+    console.log(tellJoke)
     console.log(pull);
+    return pull;
 }
