6db7b1e7d600d2288cd656e7146e70db1c179a25
Jeff Pitcher <pit19004@byui.edu>
2022-05-31 17:07:53 -0600

    finally got jokes to appear on screen

--- DadJokeGenerator/dadJokeGenerator.js
+++ DadJokeGenerator/dadJokeGenerator.js
@@ -1,18 +1,27 @@
 'use strict';
 
+const myFetch = 'https://icanhazdadjoke.com/';
 
-function tellJoke() {
+function tellJoke(joke) {
     const display = document.querySelector("#displayJoke");
     // console.log(display)
-    display.append(fetchJoke())
+    display.append(joke.joke)
 }
 
 async function fetchJoke() {
     // console.log("Hello World");
-    const pull = await fetch("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } })
-        // const url = "https://dad-jokes.p.rapidapi.com/random/joke";
-        // const pull = await fetch(url, { mode: "no-cors" });
-    const tellJoke = tellJoke();
+    // const pull = await fetch("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } })
+    // const url = "https://dad-jokes.p.rapidapi.com/random/joke";
+    // const pull = await fetch(url, { mode: "no-cors" });
+    const options = {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+
+    const pull = await fetch(myFetch, options)
+    const joke = await pull.json();
+    tellJoke(joke);
     console.log(tellJoke)
     console.log(pull);
     return pull;
